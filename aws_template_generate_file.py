8bcf1c6a2166abd2efdf4d9096944e039fca5877
Jiawei Zhu <jwzhu@sonicwall.com>
2023-08-23 15:18:15 +0800

    aws auto update script

---
+++ aws_template_generate_file.py
@@ -0,0 +1,210 @@
+import os
+import botocore.session
+import json
+
+REGION_LIST = [
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2",
+    "ap-east-1",
+    "ap-south-1",
+    "ap-northeast-3",
+    "ap-northeast-2",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-northeast-1",
+    "ca-central-1",
+    "eu-central-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "eu-north-1",
+    "me-south-1",
+    "sa-east-1"
+]
+
+Filters=[
+    {
+        'Name': 'name',
+        'Values': [
+            'SonicWall*',
+        ]
+    },
+    {
+        'Name': 'product-code.type',
+        'Values': [
+            'marketplace',
+        ]
+    },
+]
+
+class GenerateFile:
+    def __init__(self) -> None:
+        self.session = botocore.session.get_session()
+        self.mappings = {}
+        self.mappings_to_json = ""
+        self.CF_NEW_VPC_TEMPLATE = ""
+        self.CF_EXISTING_VPC_TEMPLATE = ""
+
+    def generate_mapping(self):
+        for region in REGION_LIST:
+            client = self.session.create_client("ec2", region_name=region)
+            response = client.describe_images(Filters=Filters)
+            self.mappings[region] = list()
+            for i in range(len(response["Images"])):
+                img = response["Images"][i]
+                if "DeprecationTime" not in img:
+                    continue
+                self.mappings[region].append({
+                    "Description" : img["Description"].replace('_', ''),
+                    "ImageId" : img["ImageId"],
+                })
+            self.mappings[region].sort(key=lambda d : d["Description"])
+        # for test single version
+        # self.mappings['us-east-2'].append({
+        #     "Description" : "SonicWallNSvR9999",
+        #     "ImageId" : "ami-9999",
+        # })
+
+    def generate_mapping_from_json(self, file) -> dict:
+        res_mapping = {}
+        f_content = ""
+        with open(os.path.join('./sonicwall-nsv-aws-cf-templates', file), 'r') as f:
+            f_content = f.read()
+        # print(f_content)
+        json_f = json.loads(f_content)
+        if "Mappings" not in json_f:
+            return res_mapping
+        for region_name, region in json_f["Mappings"]["RegionMap"].items():
+            res_mapping[region_name] = list()
+            for imgD, imgId in region.items():
+                # print(imgD, imgId)
+                res_mapping[region_name].append({
+                    "Description" : imgD,
+                    "ImageId" : imgId,
+                })
+        return res_mapping
+    
+    def cmp_over_version(self, old_version, new_version, is_new) -> str:
+        ret = f"{'Add New' if is_new else 'Delete Old'} version:\n"
+        new_regions = []
+        new_imgs = []
+        for region_name, region in new_version.items():
+            if region_name not in old_version:
+                new_regions.append((region_name, region))
+            else:
+                for new_img in region:
+                    flag = True
+                    for old_img in old_version[region_name]:
+                        if new_img['Description'] == old_img['Description']:
+                            if new_img['ImageId'] != old_img['ImageId']:
+                                new_imgs.append((region_name, new_img['Description'], new_img['ImageId']))
+                            flag = False
+                            break
+                    if flag is True:
+                        new_imgs.append((region_name, new_img['Description'], new_img['ImageId']))
+        if len(new_regions) == 0 and len(new_imgs) == 0:
+            ret += f"\tNo {'new' if is_new else 'old'} versions\n"
+        else:
+            if len(new_regions) != 0:
+                for region_name, region in new_regions:
+                    for img in region:
+                        ret += f"\tregion:{region_name}, Description:{img['Description']}, ImageId:{img['ImageId']}\n"
+            if len(new_imgs) != 0:
+                for region_name, imgD, imgId in new_imgs:
+                    ret += f"\tregion:{region_name}, Description:{imgD}, ImageId:{imgId}\n"
+        return ret
+
+    def cmp_versions(self, old_version : dict) -> str:
+        new_version = self.mappings
+        ret = self.cmp_over_version(old_version, new_version, True)
+        ret += self.cmp_over_version(new_version, old_version, False)
+        return ret
+
+    def transfer_mapping_to_json(self):
+        cnt = 0
+        self.mappings_to_json += '    "Mappings" : {\n        "RegionMap" : {\n'
+        for region, li in self.mappings.items():
+            self.mappings_to_json += f'{" " * 12}"{region}" : {{\n'
+            for i in range(len(li)):
+                dic = li[i]
+                if i == len(li) - 1:
+                    self.mappings_to_json += f'{" " * 16}"{dic["Description"]}" : "{dic["ImageId"]}"\n'
+                else:
+                    self.mappings_to_json += f'{" " * 16}"{dic["Description"]}" : "{dic["ImageId"]}",\n'
+            if cnt == len(self.mappings) - 1:
+                self.mappings_to_json += f'{" " * 12}}}\n'
+            else:
+                self.mappings_to_json += f'{" " * 12}}},\n'
+            cnt += 1
+        self.mappings_to_json += '        }\n    },\n'
+
+    def find_mappings_pos(self, lis) -> tuple:
+        st, en = -1, -1
+        for i, li in enumerate(lis):
+            if 'Description' in li:
+                st = i
+            elif st != -1 and 'Metadata' in li:
+                en = i
+                break
+        return st, en
+    
+    def find_amiId_pos(self, lis) -> tuple:
+        st, en = -1, -1
+        for i, li in enumerate(lis):
+            if i > 0 and 'AmiId' in lis[i - 1] and 'Description' in li:
+                st = i
+            elif st != -1 and 'AvailabilityZone' in li:
+                en = i - 1
+                break
+        return st, en
+    
+    def find_imageId_pos(self, lis) -> int:
+        pos = -1
+        for i, li in enumerate(lis):
+            if i > 0 and 'ImageId' in lis[i - 1]:
+                pos = i
+                break
+        return pos
+
+    def template_file_update(self, file) -> str:
+        content = None
+        with open(os.path.join('./sonicwall-nsv-aws-cf-templates', file), 'r') as f:
+            content = f.readlines()
+        
+        if content is None:
+            print(f'open file {file} failed!')
+            return None
+        mappings_st, mappings_en = self.find_mappings_pos(content)
+        if mappings_st == -1 or mappings_en == -1:
+            return None
+        content = content[:mappings_st + 1] + [self.mappings_to_json] + content[mappings_en:]
+
+        amiId_st, amiId_en = self.find_amiId_pos(content)
+        if amiId_st == -1 or amiId_en == -1:
+            return None
+        content = content[:amiId_st + 1] + [f'            "AllowedValues": {json.dumps([dic["Description"].replace("_", "") for dic in max(list(self.mappings.values()), key=lambda x : len(x))])},\n', '            "Type": "String"\n'] + content[amiId_en:]
+
+        imageId_pos = self.find_imageId_pos(content)
+        if imageId_pos == -1:
+            return None
+        content = content[:imageId_pos] + ['                    "Fn::FindInMap" : [ "RegionMap", { "Ref" : "AWS::Region" }, {"Ref" : "AmiId"}]\n'] + content[imageId_pos + 1:]
+        return ''.join(content)
+
+    def generate_files(self):
+        self.generate_mapping()
+        self.transfer_mapping_to_json()
+        self.CF_NEW_VPC_TEMPLATE = self.template_file_update('single-ami/cf-new-vpc.template')
+        self.CF_EXISTING_VPC_TEMPLATE = self.template_file_update('single-ami/cf-existing-vpc.template')
+
+if __name__ == '__main__':
+    from aws_template_git import HandleGit
+    native_path = './sonicwall-nsv-aws-cf-templates'
+    generateFile = GenerateFile()
+    generateFile.generate_files()
+    handleGit = HandleGit()
+    handleGit.rewrite_file(os.path.join(native_path, "single-ami/cf-existing-vpc.template"), generateFile.CF_EXISTING_VPC_TEMPLATE)
+    handleGit.rewrite_file(os.path.join(native_path, "single-ami/cf-new-vpc.template"), generateFile.CF_NEW_VPC_TEMPLATE)
+    # print(generateFile.CF_NEW_VPC_TEMPLATE)
+    # print(generateFile.CF_EXISTING_VPC_TEMPLATE)
---
+++ aws_template_git.py
@@ -0,0 +1,111 @@
+from github import Auth
+from github import Github
+from datetime import datetime
+import os
+import git
+
+from aws_template_generate_file import GenerateFile
+
+MAIN_BRANCH = 'main'
+
+
+class HandleGithub:
+    def __init__(self, token, repo_name, base_repo_name=None) -> None:
+        self.token = token
+        self.auth = Auth.Token(self.token)
+        self.g = Github(auth=self.auth)
+        self.repo = self.g.get_repo(repo_name)
+        if base_repo_name is not None:
+            self.base_repo = self.g.get_repo(base_repo_name)
+
+    def update_file(self, file_name, branch, commit_title, update_content):
+        file_on_github = self.repo.get_contents(file_name, branch)
+        self.repo.update_file(file_on_github.path, commit_title, update_content, file_on_github.sha, branch)
+        return file_on_github
+
+    def send_pull_request_to_base_repo(self, pull_request_title, pull_request_comment, head_branch, base_branch):
+        pr = self.base_repo.create_pull(title=pull_request_title, body=pull_request_comment, head=head_branch, base=base_branch)
+        return pr
+    
+class HandleGit:
+    def __init__(self) -> None:
+        self.work_path = ""
+        self.repo = None
+        self.git = None
+
+    def set_git_config(self, email, name):
+        self.git.config('--global', 'user.email', email)
+        self.git.config('--global', 'user.name', name)
+
+    def get_repo(self, repo_url, save_path):
+        if os.path.exists(save_path):
+            self.repo = git.Repo(save_path)
+        else:
+            self.repo = git.Repo.clone_from(url=repo_url, to_path=save_path)
+        self.git = self.repo.git
+        self.work_path = save_path
+
+    def switch_to_work_branch(self, branch):
+        if self.git.branch().find(branch) == -1:
+            self.git.checkout('-b', branch, "origin/" + MAIN_BRANCH)
+        else:
+            self.git.checkout(branch)
+
+    def upload_to_remote_repo(self, mes) -> bool:
+        if self.repo.is_dirty() is True:
+            self.git.add('.')
+            self.git.commit('-m', mes)
+            self.git.push('origin', 'HEAD')
+            return True
+        else:
+            return False
+
+    def set_remote_repo(self, push_repo, fetch_repo):
+        self.git.remote("set-url", "origin", fetch_repo)
+        self.git.remote("set-url", "--push" ,"origin", push_repo)
+
+    def sync_remote_repo(self):
+        self.git.fetch()
+
+    def rewrite_file(self, filename, content):
+        with open(filename, 'w') as f:
+            f.write(content)
+
+if __name__ == '__main__':
+    token = os.getenv('GITHUB_TOKEN')
+    push_url = os.getenv('GITHUB_PUSH_URL')
+    fetch_url = os.getenv('GITHUB_FETCH_URL')
+    repo = push_url[15:-4]
+    base_repo = fetch_url[15:-4]
+    user = repo.split('/')[0]
+    base_user = base_repo.split('/')[0]
+    print(repo, base_repo, user, base_user)
+    native_path = './sonicwall-nsv-aws-cf-templates'
+    git_config_user = os.getenv('GIT_CONFIG_USER')
+    git_config_email = os.getenv('GIT_CONFIG_EMAIL')
+    work_branch_prefix = os.getenv('WORK_BRANCH')
+    work_branch = work_branch_prefix + datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+    handleGithub = HandleGithub(token, repo, base_repo)
+    handleGit = HandleGit()
+    handleGit.get_repo(fetch_url, native_path)
+
+    generateFile = GenerateFile()
+    generateFile.generate_files()
+    
+    handleGit.set_git_config(git_config_email, git_config_user)
+    handleGit.set_remote_repo(push_url, fetch_url)
+    handleGit.sync_remote_repo()
+    handleGit.switch_to_work_branch(work_branch)
+    old_version = generateFile.generate_mapping_from_json("single-ami/cf-existing-vpc.template")
+    msg = generateFile.cmp_versions(old_version)
+    print(msg)
+    handleGit.rewrite_file(os.path.join(native_path, "single-ami/cf-existing-vpc.template"), generateFile.CF_EXISTING_VPC_TEMPLATE)
+    handleGit.rewrite_file(os.path.join(native_path, "single-ami/cf-new-vpc.template"), generateFile.CF_NEW_VPC_TEMPLATE)
+    if handleGit.upload_to_remote_repo(msg) is True:
+        pr = handleGithub.send_pull_request_to_base_repo("Update NSv AMI ID", msg, user + ":" + work_branch, MAIN_BRANCH)
+        print(pr)
+        pass
+    else:
+        print('No need to send pull request')
+    
